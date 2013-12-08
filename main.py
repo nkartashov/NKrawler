0ec4b7b60c80b63c487f077c4f2047af5773d81d
Nikita Kartashov <snailandmail@gmail.com>
2013-12-09 03:48:41 +0400

    Commented test code, added stdin input

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from index import Index
 from crawler import CustomCrawler
 
 
-#url = input("Input desired url")
-#depth = int(input("Input desired depth"))
+url = input("Input desired url")
+depth = int(input("Input desired depth"))
 
 #url = 'http://mit.spbau.ru/sewiki/index.php/SE_Wiki'
-url = 'http://en.wikipedia.org/wiki/World_Trade_Organization'
+#url = 'http://en.wikipedia.org/wiki/World_Trade_Organization'
+
 
 def build_index(url, depth):
     index = Index()
@@ -22,16 +23,16 @@ def build_index(url, depth):
     return index
 
 
-depth = 3
+#depth = 3
 index = build_index(url, depth)
 
 index.sort()
 
-for word in index._words.iterkeys():
-    print(word)
+#for word in index._words.iterkeys():
+#    print(word)
 
 while True:
-    query = raw_input('AMA: ').decode(sys.stdin.encoding)
+    query = raw_input('Ask Me Anything: ').decode(sys.stdin.encoding)
     if query == 'mischief managed':
         break
     result = index.query(query)
